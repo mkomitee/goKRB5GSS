2c8837f195657fc5dd6a7d30464a4b08174b2d29
Michael Komitee <mkomitee@gmail.com>
2014-08-29 01:06:37 -0400

    Some progress has been made
    Generating contexts and principals.
    Parsing & unparsing principals.
    Generating useful error messages.
    Possibly (havent tested) handling aname2lname.

--- src/example/example.go
+++ src/example/example.go
@@ -5,11 +5,30 @@ import (
 	"log"
 )
 
+const ipname = "user@EXAMPLE.COM"
+
 func main() {
 	ctx, err := kerberos.NewContext()
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
+	defer ctx.Free()
 	log.Printf("Got %+v", *ctx)
-	ctx.FreeContext()
+
+	princ, err := ctx.NewPrincipal(ipname)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	defer ctx.FreePrincipal(princ)
+	log.Printf("Got %+v", *princ)
+
+	opname, err := ctx.Unparse(princ)
+	log.Printf("Got %+v", opname)
+
+	if ipname != opname {
+		log.Printf("%+v != %+v!", ipname, opname)
+	}
+
+	lname, err := ctx.Localname(princ)
+	log.Printf("Got %+v", lname)
 }
--- src/kerberos/kerberos.go
+++ src/kerberos/kerberos.go
@@ -1,20 +1,13 @@
 package kerberos
 
+import "unsafe"
+
 // #cgo LDFLAGS: -lkrb5 -lk5crypto -lcom_err
 // #include <krb5.h>
 import "C"
 
-type Error struct {
-	Code int
-	Msg string
-}
-
-func (e Error)Error() string {
-	return e.Msg
-}
-
 type Context struct {
-	kctx C.krb5_context
+	ctx C.krb5_context
 }
 
 func NewContext() (*Context, error) {
@@ -23,14 +16,71 @@ func NewContext() (*Context, error) {
 	if code != 0 {
 		return nil, Error{
 			Code: int(code),
-			Msg: "Cannot initialize Kerberos Context"}
+			Msg:  "Cannot initialize Kerberos Context"}
 	}
-	return &Context{kctx: ctx}, nil
+	return &Context{ctx: ctx}, nil
 }
 
-func (c *Context) FreeContext() {
-	if c.kctx != nil {
-		C.krb5_free_context(c.kctx)
-		c.kctx = nil
+func (c *Context) Free() {
+	if c.ctx != nil {
+		C.krb5_free_context(c.ctx)
+		c.ctx = nil
 	}
+}
+
+func (c *Context) newError(code C.krb5_error_code) error {
+	msg := C.krb5_get_error_message(c.ctx, code)
+	defer C.krb5_free_error_message(c.ctx, msg)
+	return Error{Code: int(code), Msg: C.GoString(msg)}
+}
+
+func (c *Context) NewPrincipal(pname string) (*Principal, error) {
+	var princ C.krb5_principal
+	pnameC := C.CString(pname)
+	defer C.free(unsafe.Pointer(pnameC))
+	code := C.krb5_parse_name(c.ctx, pnameC, &princ)
+	if code != 0 {
+		return nil, c.newError(code)
+	}
+	return &Principal{princ: princ}, nil
+}
+
+func (c *Context) FreePrincipal(princ *Principal) {
+	if princ != nil {
+		C.krb5_free_principal(c.ctx, princ.princ)
+		princ.princ = nil
+	}
+}
+
+func (c *Context) Localname(p *Principal) (string, error) {
+	var buf *C.char
+	defer C.free(unsafe.Pointer(buf))
+	code := C.krb5_aname_to_localname(c.ctx, p.princ, 255, buf)
+	if code != 0 {
+		return "", c.newError(code)
+	}
+	return C.GoString(buf), nil
+}
+
+func (c *Context) Unparse(p *Principal) (string, error) {
+	var buf *C.char
+	defer C.free(unsafe.Pointer(buf))
+	code := C.krb5_unparse_name(c.ctx, p.princ, &buf)
+	if code != 0 {
+		return "", c.newError(code)
+	}
+	return C.GoString(buf), nil
+}
+
+type Error struct {
+	Code int
+	Msg  string
+}
+
+func (e Error) Error() string {
+	return e.Msg
+}
+
+type Principal struct {
+	princ C.krb5_principal
 }
